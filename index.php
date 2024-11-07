ad63fa4b1df91f28edcd0fbbd221bad04cf13d0d
Lilislaia <lilisastrialaia@gmail.com>
2024-11-07 11:03:38 +0700

    Pertemuan 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Lilis";
+$umur = 19;
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>contoh variabel PHP</title>
+    </header>
+    <body>
+        <h1>halo,  <?php echo $nama; ?>!</h1>
+        <p>$umur anda adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html>
